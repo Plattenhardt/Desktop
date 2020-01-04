f6e6d49feb027b9bdd9b157437e8c2ea9ee7644f
Plattenhardt <kauanov.aslan@gmail.com>
2020-01-04 17:05:42 +0600

    123

---
+++ Projects/Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public float pervii { get; set; }
+        public float vtoroi { get; set; }
+
+        public string op { get; set; }
+
+        public float result { get; set; }
+
+        public static float Operation(float x, string op, float y)
+        {
+            switch (op)
+            {
+                case "+":
+                return x + y;
+                break;
+                case "-":
+                return x - y;
+                break;
+                case "*":
+                return x * y;
+                break;
+                case "/":
+                return x / y;
+                break;
+
+
+
+
+            }
+            return x+y;
+          }
+    }
+}
---
+++ Projects/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.pervii = Convert.ToInt32(Console.ReadLine());
+            calculator.op = Convert.ToString(Console.ReadLine());
+            calculator.vtoroi = Convert.ToInt32(Console.ReadLine());
+
+            
+            float res = Calculator.Operation(calculator.pervii, calculator.op, calculator.vtoroi);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
